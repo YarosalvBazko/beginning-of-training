9c06547be70ef05e11803413f25f390c8834403c
Yaroslav Bazko <ybazko08@gmail.com>
2026-08-26 18:23:05 +0300

    Добавлен pages/cookies.php

---
+++ pages/cookies.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Cookie в PHP";
+require_once "../blocks/header.php";
+require_once "../includes/DemoHelper.php";
+?>
+
+<h1 class="section-title">🍪 Cookie в PHP</h1>
+
+<p class="lead">Установка, чтение и удаление cookie.</p>
+
+<a href="/index.php" class="back-link">← Назад к списку</a>
+
+<?php
+
+?>
+
+<?php require_once "../blocks/footer.php"; ?>
